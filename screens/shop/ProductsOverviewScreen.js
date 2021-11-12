b265aa3673c0180b8a42b153f990ea6f2dabf4b7
amyfang3 <afang@uship.com>
2021-11-12 11:11:54 -0600

    feat: initial setup

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = (props) => {
+  return (
+    <FlatList />
+  );
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
