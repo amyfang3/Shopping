03a3b1de5cd3dc92a076b1257967018b4fb78b92
amyfang3 <afang@uship.com>
2021-11-12 14:23:12 -0600

    feat: formatting

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 import ShopNavigator from './navigation/ShopNavigator';
@@ -11,7 +13,22 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+//   });
+// };
+
 export default function App() {
+  const [isFontLoaded] = Font.useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!isFontLoaded) {
+    return <AppLoading />;
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
