56eca77563f8a02428ec1bd4667421203065907d
amyfang3 <afang@uship.com>
2021-11-12 11:40:05 -0600

    feat: set up initial navigation

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -13,16 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View />
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const ProductsOverviewScreen = (props) => {
+  // state.products from rootReducer in App.js
+  const products = useSelector((state) => state.products.availableProducts);
   return (
-    <FlatList />
+    <FlatList
+      data={products}
+      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+    />
   );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products',
 };
 
 export default ProductsOverviewScreen;
